028f3f7ec3c46185cb96b42dd3f76b10eb9a5430
Christian Worthen <110493480+cwort40@users.noreply.github.com>
2023-08-01 13:30:37 -0700

    added files for creation of user class and auth

--- app/__init__.py
+++ app/__init__.py
@@ -3,10 +3,11 @@ from flask import Flask
 from blueprints.black_scholes_calculator import black_scholes_calculator
 from blueprints.portfolio_risk_analysis import portfolio_risk_calculator
 
+
 app = Flask(__name__, template_folder='../templates')
 
+# Register blueprints
 app.register_blueprint(black_scholes_calculator)
-
 app.register_blueprint(portfolio_risk_calculator)
 
 if __name__ == '__main__':
