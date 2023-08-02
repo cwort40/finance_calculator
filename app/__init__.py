55c6b5187a15b6935c8a6accdec431523007838f
Christian Worthen <110493480+cwort40@users.noreply.github.com>
2023-08-02 13:31:18 -0700

    implemented basic functionality for google oauth

--- app/__init__.py
+++ app/__init__.py
@@ -1,14 +1,26 @@
-from flask import Flask
-
+from flask import Flask, render_template
+from blueprints.auth import auth
 from blueprints.black_scholes_calculator import black_scholes_calculator
 from blueprints.portfolio_risk_analysis import portfolio_risk_calculator
+import os
 
 
-app = Flask(__name__, template_folder='../templates')
+def create_app():
+    app = Flask(__name__, template_folder='../templates')
+    app.secret_key = os.environ.get('SECRET_KEY')  # Make sure to set a SECRET_KEY
 
-# Register blueprints
-app.register_blueprint(black_scholes_calculator)
-app.register_blueprint(portfolio_risk_calculator)
+    # Register blueprints
+    app.register_blueprint(auth, url_prefix='/auth')
+    app.register_blueprint(black_scholes_calculator)
+    app.register_blueprint(portfolio_risk_calculator)
+
+    @app.route('/')
+    def home():
+        return render_template('home.html')  # Render the home page template
+
+    return app
+
 
 if __name__ == '__main__':
+    app = create_app()
     app.run(debug=True)
---
+++ app/blueprints/auth.py
@@ -0,0 +1,70 @@
+from flask import Blueprint, redirect, url_for, session, request, render_template
+from functools import wraps
+from flask_oauthlib.client import OAuth
+import os
+
+auth = Blueprint('auth', __name__)
+
+oauth = OAuth(auth)
+
+google = oauth.remote_app(
+    'google',
+    consumer_key=os.environ.get('YOUR_GOOGLE_CLIENT_ID'),
+    consumer_secret=os.environ.get('YOUR_GOOGLE_CLIENT_SECRET'),
+    request_token_params={'scope': 'email'},
+    base_url='https://www.googleapis.com/oauth2/v1/userinfo',
+    request_token_url=None,
+    access_token_method='POST',
+    access_token_url='https://accounts.google.com/o/oauth2/token',
+    authorize_url='https://accounts.google.com/o/oauth2/auth',
+)
+
+
+@auth.route('/login')
+def login():
+    return render_template('login.html')  # Render the login page
+
+
+@auth.route('/google_login')
+def google_login():
+    return google.authorize(callback=url_for('auth.login_callback', _external=True))
+
+
+@auth.route('/logout')
+def logout():
+    session.pop('user')
+    return redirect(url_for('home'))  # Redirect to your home route
+
+
+@auth.route('/login/callback')
+def login_callback():
+    response = google.authorized_response()
+    if response is None or response.get('access_token') is None:
+        # Handle login error
+        return 'Access denied: reason={} error={}'.format(
+            request.args['error_reason'],
+            request.args['error_description']
+        )
+
+    session['google_token'] = (response['access_token'], '')
+    user_info = google.get('userinfo')
+    session['user'] = user_info.data  # You can store user info in session
+
+    return redirect(url_for('home'))  # Redirect to the home route
+
+
+def login_required(f):
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if 'user' not in session:
+            return redirect(url_for('auth.login'))
+        return f(*args, **kwargs)
+
+    return decorated_function
+
+
+@google.tokengetter
+def get_google_oauth_token():
+    return session.get('google_token')
+
+# TODO: implement session tokens for API
